966018a6487bc0cf72aeb203537862d96704bffb
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-09-17 20:22:57 -0300

    1 pagina ok

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,16 +1,19 @@
 "use client"
 
-const PageWrapper = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
+import Image from 'next/image'
+import styles from './page.module.css'
+import { FilterBar } from '@/components/filter-bar'
+import { ProductsList } from '@/components/products-list'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
-
+  const client = new QueryClient();
   return (
-    <div>
-      aqui
-    </div>
+    <QueryClientProvider client={client}>
+      <main className={styles.main}>
+        <FilterBar/>
+        <ProductsList/>
+      </main>
+    </QueryClientProvider>
   )
 }
