af61088f55ab3579b5ada40c8fb8aaf1482f566c
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-09-17 10:30:28 -0300

    iniciando

---
+++ capputeeno/src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+const PageWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export default function Home() {
+
+  return (
+    <div>
+      aqui
+    </div>
+  )
+}
